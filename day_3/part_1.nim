1eb2bfac17d7a8f78a950e3fe4db07b26546b8c7
Isaac Naylor <i.acnaylor@gmail.com>
2023-12-03 11:34:20 +0000

    Day 3 - Part 1

--- day_3/part_1.nim
+++ day_3/part_1.nim
@@ -1,21 +1,22 @@
 import regex
 import strformat
-import sequtils
 import math
 import strutils
-import sugar
 
 const
-  input = staticRead("./sample.txt")
+  input = staticRead("./input.txt")
 
 type Symbol = ref object
   width, row, col: int
 
-func gridPos(symbol: Symbol): int =
+proc `$`*(s: Symbol): string =
+  fmt"Symbol(row: {s.row}, col: {s.col})"
+
+func gridPos*(symbol: Symbol): int =
   (symbol.row * symbol.width) + symbol.col
 
 proc findSymbols(data: string, rowWidth: int): seq[Symbol] =
-  const pattern = re2 "[*#+%=/@$-&]"
+  const pattern = re2 "[^.0-9]"
   var
     lastFoundPos = 0
     match: RegexMatch2
@@ -29,21 +30,39 @@ proc findSymbols(data: string, rowWidth: int): seq[Symbol] =
 
     lastFoundPos = match.boundaries.a + 1
 
-proc solve(data: string): int =
+proc isAdjacentTo(symbol: Symbol; pos: tuple[l, r, c: int]): bool =
+  if symbol.row - 1 <= pos.r and symbol.row + 1 >= pos.r:
+    let
+      lowest = pos.c - 1
+      highest = pos.c + pos.l + 1
+
+    if symbol.col >= lowest and symbol.col <= highest:
+      return true
+
+  false
+
+proc solve(input: string): int =
   let
-    grid = data.splitLines.map((line: string) => line.strip(true, true)).collect()
-    width = grid[0].len()
+    data = input.splitLines().join("")
+    width = input.splitLines[0].strip(true, true).len()
     symbols = findSymbols(data, width)
+    pattern = re2 "([0-9]{1,3})"
 
-  var idx = 0
-  while idx < data.len():
-    if data[idx] == '.':
-      idx += 1
-      continue
+  var
+    lastMatchPos = 0
+    match: RegexMatch2
 
-    idx += 1
+  while find(data, pattern, match, lastMatchPos):
+    let
+      length = match.boundaries.b - match.boundaries.a
+      row = int floor(match.boundaries.a / width)
+      col = match.boundaries.a - (width * row)
 
-  1
+    for symbol in symbols:
+      if symbol.isAdjacentTo((l: length, r: row, c: col)):
+        result += parseInt data[match.group 0]
+        break
 
+    lastMatchPos = match.boundaries.b+1
 
 echo "Day 3 - Part 1: ", solve(input)
