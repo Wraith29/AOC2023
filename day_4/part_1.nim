1a431dc7790c52db6825be50dba7aafbd366b280
Isaac Naylor <i.acnaylor@gmail.com>
2023-12-04 07:34:18 +0000

    Day 4 - Part 1 / Part 2 partial impl

---
+++ day_4/part_1.nim
@@ -0,0 +1,63 @@
+import math
+import regex
+import sequtils
+import strformat
+import strutils
+import sugar
+
+const
+  input = staticRead("./input.txt")
+
+type Card = ref object
+  id: int
+  winningNumbers, inputNumbers: seq[int]
+
+proc newCard(id: int; winningNumbers, inputNumbers: seq[int]): Card =
+  Card(id: id, inputNumbers: inputNumbers, winningNumbers: winningNumbers)
+
+proc `$`*(c: Card): string =
+  fmt"Card(cardId: {c.id}, winningNumbers: {c.winningNumbers}, inputNumbers: {c.inputNumbers})"
+
+proc value(c: Card): int =
+  for num in c.inputNumbers:
+    if num in c.winningNumbers:
+      if result == 0:
+        result = 1
+      else:
+        result *= 2
+
+proc solve(data: string): int =
+  const pattern = re2 "Card\\s*(?P<cardId>[0-9]*):\\s(?P<winningNumbers>[0-9\\s]*)\\s\\|\\s(?P<inputNumbers>[0-9\\s]*)"
+
+  var
+    cards = newSeq[Card]()
+    m: RegexMatch2
+  
+  for line in data.splitLines:
+    if not line.find(pattern, m):
+      ## Shouldn't ever happen but JIC
+      continue
+
+    let
+      cardId = line[m.group("cardId")]
+      winningNumbers = line[m.group("winningNumbers")]
+      inputNumbers = line[m.group("inputNumbers")]
+
+    cards.add(
+      newCard(
+        parseInt cardId,
+        winningNumbers
+          .split(' ')
+          .filter((num: string) => num.len() > 0)
+          .map((num: string) => parseInt strip(num, true, true)),
+        inputNumbers
+          .split(' ')
+          .filter((num: string) => num.len() > 0)
+          .map((num: string) => parseInt strip(num, true, true))
+      )
+    )
+  
+  cards.map((c: Card) => c.value()).sum()
+
+echo "Day 4 - Part 1: ", solve(input)
+
---
+++ day_4/part_2.nim
@@ -0,0 +1,79 @@
+## Wrong Answers
+## 
+## 5756587
+
+import tables
+import regex
+import sequtils
+import strformat
+import strutils
+import sugar
+
+const
+  input = staticRead("./sample.txt")
+
+type Card = ref object
+  id: int
+  winningNumbers, inputNumbers: seq[int]
+
+proc newCard(id: int; winningNumbers, inputNumbers: seq[int]): Card =
+  Card(id: id, inputNumbers: inputNumbers, winningNumbers: winningNumbers)
+
+proc `$`*(c: Card): string =
+  fmt"Card(cardId: {c.id}, winningNumbers: {c.winningNumbers}, inputNumbers: {c.inputNumbers})"
+
+proc winCount(c: Card): int =
+  for n in c.inputNumbers:
+    if n in c.winningNumbers:
+      result += 1
+
+proc getCards(data: string): seq[Card] =
+  const pattern = re2 "Card\\s*(?P<cardId>[0-9]*):\\s(?P<winningNumbers>[0-9\\s]*)\\s\\|\\s(?P<inputNumbers>[0-9\\s]*)"
+
+  var
+    m: RegexMatch2
+  
+  for line in data.splitLines:
+    if not line.find(pattern, m):
+      ## Shouldn't ever happen but JIC
+      continue
+
+    let
+      cardId = parseInt line[m.group("cardId")]
+      winningNumbers = line[m.group("winningNumbers")]
+      inputNumbers = line[m.group("inputNumbers")]
+      card = newCard(
+        cardId,
+        winningNumbers
+          .split(' ')
+          .filter((num: string) => num.len() > 0)
+          .map((num: string) => parseInt strip(num, true, true)),
+        inputNumbers
+          .split(' ')
+          .filter((num: string) => num.len() > 0)
+          .map((num: string) => parseInt strip(num, true, true))
+      )
+    
+    result.add card
+
+proc solve(data: string): int =
+  let cards = getCards(data)
+  var cardTable = newTable[int, int]() ## Id / Count
+
+  for i in 1 .. cards.len():
+    cardTable[i] = 1
+
+  ## The result needs to be the total number of cards played
+  ## For each card, set the next `n` cards to be +1
+  ## Each subsequent card needs to be increased by that amount
+
+  for card in cards:
+    echo cardTable
+    for i in card.id+1 ..< card.id+1+card.winCount():
+      cardTable[i] += (i * cardTable[card.id])
+
+  for v in cardTable.values():
+    result += v
+
+echo "Day 4 - Part 2: ", solve(input)
+
