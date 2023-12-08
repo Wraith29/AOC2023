5b2cd17e87d4d3a5283ade1e3a4155e46d3c17f6
Isaac Naylor <i.acnaylor@gmail.com>
2023-12-08 12:01:30 +0000

    Added Day 6

---
+++ src/day_6/part_1.nim
@@ -0,0 +1,33 @@
+import strutils
+import sequtils
+import ../timing
+import sugar
+
+const input = staticRead("./sample.txt")
+
+type
+  Race = object
+    time, distance: int
+
+proc solve(data: string): int =
+
+  let
+    newLineSplit = data.split(Newlines).filter((s: string) => not s.isEmptyOrWhitespace)
+    timeLine = newLineSplit[0]
+      .split(Whitespace)[1..^1]
+      .filter((s: string) => not s.isEmptyOrWhitespace)
+      .map((s: string) => parseInt s)
+    distLine = newLineSplit[1]
+      .split(Whitespace)[1..^1]
+      .filter((s: string) => not s.isEmptyOrWhitespace)
+      .map((s: string) => parseInt s)
+    races = collect:
+      for (time, dist) in zip(timeLine, distLine):
+        Race(time: time, distance: dist)
+  
+  echo timeLine, ", ", distLine
+  
+  echo races
+
+
+timeit(6, 1, input, solve)
